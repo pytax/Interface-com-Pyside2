617575aaa3a008dad07b52ba63d92a541d1af9f3
pytax <na.automatizacao@gmail.com>
2021-06-02 11:53:48 -0300

    alterando arquivo main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+print('teste')
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,4 +14,3 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-g
